f170708b681fd8d51d8167db82b3abde46e873d7
pjcrann219 <122841400+crannp@users.noreply.github.com>
2024-06-14 10:11:31 -0400

    finished application and part2.txt for hw2

--- hw2/application.py
+++ hw2/application.py
@@ -6,7 +6,7 @@ from sklearn.datasets import make_regression
 #-------------------------------------------------------------------------
 '''
     Problem 2: Apply your Linear Regression
-    In this problem, use your linear regression method implemented in problem 1 to do the prediction.
+    In this problem, use your linear n method implemented in problem 1 to do the prediction.
     Play with parameters alpha and number of epoch to make sure your test loss is smaller than 1e-2.
     Report your parameter, your train_loss and test_loss 
     Note: please don't use any existing package for linear regression problem, use your own version.
@@ -22,21 +22,13 @@ Xtrain, Ytrain, Xtest, Ytest = X[::2], y[::2], X[1::2], y[1::2]
 
 #########################################
 
-def train_and_test(Xtrain, Ytrain, Xtest, Ytest, alpha, n_epoch):
-    w = train(Xtrain, Ytrain, alpha, n_epoch)
-    yhat = compute_yhat(Xtest, w)
-    L = compute_L(yhat,Ytest)
-    return L
+alpha = 0.001
+n_epoch = 10000
 
-n_epochs = np.array([10, 100, 1000, 10000, 100000])
-for n_epoch in n_epochs:
-    L = train_and_test(Xtrain, Ytrain, Xtest, Ytest, 0.01, n_epoch)
-    print(f"n_epoch: {n_epoch}\tL: {L}")
-
-alphas = np.array([.1, .01, .001, .0001])
-for alpha in alphas:
-    L = train_and_test(Xtrain, Ytrain, Xtest, Ytest, alpha, 100000)
-    print(f"alpha: {alpha}\tL: {L}")
+w = train(Xtrain, Ytrain, alpha, n_epoch)
+yhat = compute_yhat(Xtest, w)
+L = compute_L(yhat,Ytest)
+print(f"Alpha = {alpha}, Num Epochs: {n_epoch}, Test Loss: {np.round(L,5)}")
 
 #########################################
 
